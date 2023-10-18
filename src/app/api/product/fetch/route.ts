666fe94a69f3ffb57997334c639ea4fb2c50705b
dipesh162 <dipeshsingh162@gmail.com>
2023-10-19 00:46:07 +0530

    Fetch single product API

---
+++ src/app/api/product/fetch/route.ts
@@ -0,0 +1,27 @@
+import {connect} from '@/dbConfig/dbConfig';
+import Product from "@/models/productModel";
+import { NextRequest, NextResponse} from 'next/server';
+
+connect()
+
+
+export async function GET(request: NextRequest){
+    try{
+
+        let searchParams = request.nextUrl.searchParams
+        let slug = searchParams.get('slug')
+
+        const product =  await Product.findOne({slug:slug})
+        if(!product){
+            return NextResponse.json({error: "Product does not exist"}, {status: 400})
+        }
+
+        return NextResponse.json({
+            product : product,
+            success:true,
+        })
+
+    } catch (err: any){
+        return NextResponse.json({error: err.message}, {status: 500})
+    }   
+}
