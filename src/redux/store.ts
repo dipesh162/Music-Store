34da2357721d33657dce2ab6b62b9ffe15cdcfcd
dipesh162 <dipeshsingh162@gmail.com>
2023-11-30 00:38:20 +0530

    Added redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist';
 import authReducer from './slices/authSlice'
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
 
-export const store = configureStore({
+// Persisting auth reducer values on refresh
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: persistedAuthReducer
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+const persistor = persistStore(store);
+export {store, persistor};
 
 // export default store
 export type RootState = ReturnType<typeof store.getState>;
