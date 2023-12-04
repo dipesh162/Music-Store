8fc63ce5668af7a73ec9e08bcfbe120f867de57f
dipesh162 <dipeshsingh162@gmail.com>
2023-12-04 14:51:27 +0530

    Updated productSchema with quantity

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,18 +1,48 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-        name: String,
-        brand: String,
-        category: String, 
-        subCategory: String, 
-        slug: String,
-        isAvailable: Boolean,
-        description: String,
-        price: Number, 
-        images: Array,
-        ratings: Number, 
+        name: {
+                type: String,
+                required: [true, 'Please provide product name']
+        },
+        brand: {
+                type: String,
+                required: [true, 'Please provide product brand']
+        },
+        category: {
+                type: String, 
+                required: [true, 'Please provide product category']
+        },
+        subCategory: {
+                type: String, 
+                required: [true, 'Please provide product subCategory']
+        },
+        quantity: {
+                type: Number,
+                required: [true, 'Please provide product quantity']
+        },
+        slug: {
+                type: String,
+                required: [true, 'Please provide product slug']
+        },
+        description: {
+                type: String,
+                required: [true, 'Please provide product description']
+        },
+        price: {
+                type: Number, 
+                required: [true, 'Please provide product price']
+        },
+        images: {
+                type: Array,
+                required: [true, 'Please provide product images']
+        },
+        ratings: {
+                type: Number, 
+                required: [true, 'Please provide product ratings']
+        },
 })
 
-const Product = mongoose.models.products || mongoose.model("products", productSchema);
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
