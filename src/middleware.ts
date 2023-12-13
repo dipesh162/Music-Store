280433009f93f501d5991e7dce654ee548732393
dipesh162 <dipeshsingh162@gmail.com>
2023-12-13 16:41:32 +0530

    ADded cors middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
+const allowedOrigins = ['https://music-store-dipesh162.vercel.app', 'http://localhost:3000', 'https://www.google.com']
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 //   return NextResponse.redirect(new URL('/home', request.url))
+    const origin = request.headers.get('origin')
+    if(origin && !allowedOrigins.includes(origin)){
+      return new NextResponse(null, {
+        status:400,
+        statusText: 'Bad Request',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+      })
+    }
+
     const path = request.nextUrl.pathname
     const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
 
