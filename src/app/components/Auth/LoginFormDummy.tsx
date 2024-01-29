0fe5d07aaa4e25b68152e4b20504d3d74380b535
dipesh162 <dipeshsingh162@gmail.com>
2024-01-29 15:58:58 +0530

    Added jest testing library

---
+++ src/app/components/Auth/LoginFormDummy.tsx
@@ -0,0 +1,10 @@
+export default function LoginFormDummy(){
+    return(
+        <>
+            <h1>Sign In</h1>
+            <button disabled={true} data-testid='d-button'>
+                Disabled Button
+            </button>
+        </>
+    )
+}
